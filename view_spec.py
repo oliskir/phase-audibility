528d0fc37463b4fe2e7ce30a752d94e1225c5e33
oliver <oliver.kirsebom@gmail.com>
2022-02-07 18:28:40 -0400

    added figures

--- view_spec.py
+++ view_spec.py
@@ -1,8 +1,19 @@
 from ketos.audio.spectrogram import MagSpectrogram
 import matplotlib.pyplot as plt
 
+
 window = 0.02
 step = 0.004
+
+
+original = MagSpectrogram.from_wav("original_fft.wav", window=window, step=step)
+modified = MagSpectrogram.from_wav("modified_fft.wav", window=window, step=step)
+
+original.plot()
+modified.plot()
+
+plt.show()
+
 
 original = MagSpectrogram.from_wav("original_stft.wav", window=window, step=step)
 recovered = MagSpectrogram.from_wav("recovered_stft.wav", window=window, step=step)
